8c106e623c5a0f81425d7c99cec3b5d24c942d55
chelseycornejo <chelsey.daguio@gmail.com>
2023-03-04 15:05:52 -0500

    started to add conditionals for if cards match or not

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,27 @@
-// Set up Firebase database, including initializing our database and our dbRef
-import firebaseInfo from './firebase.js';
-import {getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
+// TODO: 
+    // Fix conditionals for matches or non-matches. Currently card1 === card2 is not working.
 
-const database = getDatabase(firebaseInfo)
-const dbRef = ref(database);
-// const cardsRef = ref(database, '/cards');
-const ulElement = document.querySelector('ul');
+    
+    // Set up Firebase database, including initializing our database and our dbRef
+    import firebaseInfo from './firebase.js';
+    import {getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
+    
+    const database = getDatabase(firebaseInfo)
+    const dbRef = ref(database);
 
-// get(cardsRef).then((data) => {
-//     console.log(data.val())
-// });
-
-get(dbRef).then((snapshot) => {
+// Use document.querySelector() to get JS objects:
+    // create variable for div with class of cardsContainer
+    const ulElement = document.querySelector('ul');
+    // create variable for div with message updates
+    const message = document.querySelector('.message');
+    
+    // create variable for revealed key value
+    get(dbRef).then((snapshot) => {
     const data = snapshot.val();
+    // create variable for cardList - the values of the array
     const cards = data.cards;
     
+    // create variable for cardPickList - [...cardList, ...cardList]
     const cardPickList = [...cards, ...cards];
     const shuffledArray = shuffleArray(cardPickList);
     cardPickList.forEach(function(card) {
@@ -39,6 +46,7 @@ get(dbRef).then((snapshot) => {
         ulElement.appendChild(liElement);
 
     })
+    // let user preview cards at start of game for a few seconds
     const obj = setInterval(flipCards, 1000);
     let cnt = 0;
     function flipCards() {
@@ -46,7 +54,7 @@ get(dbRef).then((snapshot) => {
             const frontDiv = card.querySelector('.front');
             const backDiv = card.querySelector('.back');
 
-            if (cnt < 3) {
+            if (cnt < 8) {
                 frontDiv.style.display = 'none';
                 backDiv.style.display = 'block';
             } else {
@@ -63,43 +71,77 @@ get(dbRef).then((snapshot) => {
     }
 
     // GAME STARTS, USER NEEDS TO CHOOSE CARDS
-
     let endOfTurn = false;
     let revealedCount = 0;
-    
-    document.querySelectorAll('.card').forEach((card) => {
-        card.addEventListener('click', (event) => {
-            // if endOfTurn = false || revealed variabslale is equal to true, then return
+    let pairs = 0;
+
+// addEventListener to .card div - when clicked 
+document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('click', (event) => {
+            // Stop user from clicking revealed cards. if endOfTurn = true || is revealed
             let card1 = "";
             let card2 = "";
             if (card.dataset.revealed === 'true' || endOfTurn === true) {
                 return;
             }
+            message.textContent = '';
             const frontDiv = card.querySelector('.front');
             const backDiv = card.querySelector('.back');
             frontDiv.style.display = 'none';
             backDiv.style.display = 'block';
             card.dataset.revealed = true;
-
+            
             if (revealedCount === 0) {
+                // select card 1
                 card1 = card.getAttribute('img');
                 revealedCount++;
-                console.group(card1);
-                console.log(revealedCount);
+                console.log('this is card1' + card1);
+
             } else {
+                // select card 2
                 card2 = card.getAttribute('img');
                 revealedCount = 0;
-                console.log(card2);
+                console.log('this is card2' + card2);
                 endOfTurn = true;
-            }
+                
+                    // CONDITIONALS
+                    if (card1 === card2) {
+                        // if cards1 and card2 match
+                        // make .message div = it matched! for 3s
+                        // clear .message div
+                        // increase # of match pairs found **
+                        // cards remain faced up
+                        // set card 1 and 2 - variable revealed key value = true
+                        // decrease cardCount by 2
+                        // cardPairs += 1
+                        // revealedCount = 0
+                        message.textContent = 'That was a match!';
+                        pairs++;
+                        revealedCount = 0;
+                        endOfTurn = false;
+                        if (pairs === 6){
+                            // if cardPairs === 6
+                            // make .message div = YOU WIN! Refresh to play again
+                            message.textContent = 'YOU WIN! Refresh to play again.'
+                        }
+                        
+                    } else {
+                        // if card1 and card2 don't match
+                            // make .message div = cards do not match for 3s
+                            // clear .message div
+                            // increment the number of attempts made **
+                            // cards go faced down 
+                            // revealedCount = 0
+                        message.textContent = 'That was not a match!';
+                        revealedCount = 0;
+                        endOfTurn = false;
+                        card.dataset.revealed = false;
+                        
+                    }
+            } return;
         });
     });
 });
-
-
-
-
-
 
 // Create function to shuffle the cardPickList array
 function shuffleArray(array) {
@@ -110,89 +152,6 @@ function shuffleArray(array) {
 }
 
 
-let counter = 0;
-let firstSelection = "";
-let secondSelection = "";
-const pickArray = document.querySelectorAll('.card');
-pickArray.forEach((card) => {
-    pickArray.addEventListener('click', () => {
-        card.classList.add('clicked');
-
-        if (counter === 0) {
-            firstSelection = card.getAttribute('img'); 
-            counter++;
-        } else {
-            secondSelection = card.getAttribute('img');
-            counter = 0;
-        }
-
-        console.log(firstSelection);
-        console.log(secondSelection);        
-    })
-})
-
-// Add SDKs for Firebase products that we will use 
-// Use document.querySelector() to get JS objects:
-    // create variable for div with class of cardsContainer
-    // const cardsContainer= document.getElementsByClassName(cardsContainer)
-    // // create variable for cardList - the values of the array
-    // // create variable for cardPickList - [...cardList, ...cardList]
-    
-    
-    // create variable for cardCount - cardPickList.length
-
-    // create variable for revealed key value
-
-
-    // let revealedCount = 0; (cards being chosen (i.e., card 1, card2)
-    // let endOfTurn = false;
-    // let cardPairs = 0; (overall pairs found count)
-
-
-
-//USER PREVIEWS CARDS AT START OF GAME
-// toggle div with .front to .back so user can preview cards for 10s
-    // toggle div class back to front
-
-
-// GAME STARTS, USER NEEDS TO CHOOSE CARDS
-// CHOOSE CARD 1
-    // addEventListener to .card div - when clicked 
-        // Stop user from clicking revealed cards. if endOfTurn = false || revealed variable is equal to true, then return
-        // toggle div class from .front to .back
-            // make a variable for .card1 value
-            // change style to display: block. 
-        // revealedCount = 1
-
-// CHOOSE CARD 2
-    // addEventListener to .card div - when clicked
-        // if endOfTurn = false || revealed variabslale is equal to true, then return
-        // toggle div from .front to .back
-            // make variable for .card2 value
-            // change style to display: block
-        // revealedCount = 2
-        // endOfTurn = true
-
-    // CONDITIONALS
-    // if card1 and card2 don't match
-        // make .message div = cards do not match for 3s
-        // clear .message div
-        // increment the number of attempts made **
-        // cards go faced down 
-        // revealedCount = 0
-
-    // if cards1 and card2 match
-        // make .message div = it matched! for 3s
-        // clear .message div
-        // increase # of match pairs found **
-        // cards remain faced up
-        // set card 1 and 2 - variable revealed key value = true
-        // decrease cardCount by 2
-        // cardPairs += 1
-        // revealedCount = 0
-    // if cardPairs === 6
-        // make .message div = YOU WIN! Refresh to play again.
-        
         
 //Play Again Button
 // create variable for .restart element 
